81f163a0023e110611f0319b0dcb86231df1c2cc
susannecambi <susanne.cambi@unil.ch>
2026-03-19 14:35:32 +0100

    changed password again

--- experiment.js
+++ experiment.js
@@ -1,5 +1,5 @@
 const PCLOUD_USER = "Susanne.Cambi@unil.ch";
-const PCLOUD_PASS = "qojtir-wantoG-2vabny";
+const PCLOUD_PASS = "97531!onceuponatime";
 const PCLOUD_FOLDER_ID = 22842773391;
 let pcloudUploadPromise = null;
 const urlParams = new URLSearchParams(window.location.search);
